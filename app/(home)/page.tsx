628042d41229042fbb817f1f4496fc613c20183d
Ayesha Siddiqua <siddiquaayesha18.2003@gmail.com>
2024-10-31 11:32:02 +0530

    new

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Navbar from './components/Navbar' 
 import HeroSection from './components/HeroSection'
 import Skills from './components/Skills'
@@ -7,24 +7,30 @@ import Footer from './components/Footer'
 import Education from './components/Education'
 export default function page() {
   return (
-    <div className='min-h-screen bg-black  overflow-hidden '>
-      <div className='dark:bg-black dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
-      <div className='max-w-7xl mx-auto p-5'>
-        <Navbar/>
-        <HeroSection/>
-        
-      </div>
-      <div className='h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left0-0 xl:-bottom-14 w-full'>
+    <Suspense fallback={<Loading />}>
+      <div className='min-h-screen bg-black  overflow-hidden '>
+        <div className='dark:bg-black dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
+            <div className='max-w-7xl mx-auto p-5'>
+              <Navbar/>
+              <HeroSection/>
+            </div>
+            <div 
+            className='h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left0-0 xl:-bottom-14 w-full'>
+            </div>
+        </div>
 
+        <div className='max-w-7xl mx-auto p-5 mt-20'>
+        <Skills/>
+        <Education/>
+        <Projects/>
+        <Footer/>
+        
+        </div>
+      
       </div>
-      </div>
-      <div className='max-w-7xl mx-auto p-5 mt-20'>
-      <Skills/>
-      <Education/>
-      <Projects/>
-      <Footer/>
-      </div>
-    
-    </div>
+    </Suspense>
   )
+}
+function Loading() {
+  return <h2 className=' flex justify-center items-center relative top-80'>🌀 Loading...</h2>;
 }
